8d88f302ac27333a51344d2eb47ecac0000ac639
parkchanjae <ekzm2302@naver.com>
2022-10-13 13:13:03 +0900

    update

--- script/main.js
+++ script/main.js
@@ -2,4 +2,15 @@ $(function() {
     $("#close_banner").click(function() {
     $("#banner").addClass(d-none);       
     })
+
+    $("#cart").click(function() {
+        var order = {
+            buyItem : "라움 2인 그릇 6P",
+            buyCount: 1,
+            buyPrice: 39500
+        }
+        var userChoice = confirm("장바구니에 담았습니다, 결제창으로 이동");
+        userChoice ? location.href("/payment") : null;
+
+    })
 })
